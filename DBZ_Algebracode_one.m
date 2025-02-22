00d36d1fe4bc0cc89dade772921b33945856ba17
includemohammad <127312425+includemohammad@users.noreply.github.com>
2025-02-22 20:16:20 +0330

    Add files via upload

---
+++ DBZ_Algebracode_one.m
@@ -0,0 +1,13 @@
+clc ;
+clear all ;
+close all ;
+
+%% sub and sum vector 
+u1 = [1+1i;-5;0];
+u2 = [1 0 2]';
+sum = u1+u2 ;
+sub = u1 -u2 ; 
+%% sub and sum Marix
+A =[2 0;-1i 1]; B = [3-1i sqrt(2);0 8];
+sum = A - B 
+sub = A - B 
